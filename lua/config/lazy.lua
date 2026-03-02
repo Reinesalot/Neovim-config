fda8f058653471cd59027e4a892a39cf63071603
ReinesReines <bobmcbob717@gmail.com>
2026-03-02 19:31:40 +0000

    made some edits

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,3 +51,8 @@ require("lazy").setup({
     },
   },
 })
+
+require("nvim-treesitter").setup({
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath("data") .. "/site",
+})
